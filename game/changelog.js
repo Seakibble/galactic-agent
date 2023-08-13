6add8be3db28d38345e90de07f33de90a5a52d81
Max Leeming <max.leeming@gmail.com>
2023-08-13 14:28:37 -0400

    v0.14.10 - Fixed reticule... I hope

--- game/changelog.js
+++ game/changelog.js
@@ -1,5 +1,6 @@
 const CHANGELOG = [
     // { version: '', log: '' },
+    { version: 'v0.14.10', log: 'Fixed reticule... I hope' },
     { version: 'v0.14.9', log: 'Fixed cursors... again' },
     { version: 'v0.14.8', log: 'Fixed cursors for live site' },
     { version: 'v0.14.7', log: 'You start with gun, bulletTime upgrade' },

60fa6db20b688164274b937df607b2708965efb4
Max Leeming <max.leeming@gmail.com>
2023-08-15 15:05:00 -0400

    v0.16.4 - Re-added murderBlock.png

--- game/changelog.js
+++ game/changelog.js
@@ -1,5 +1,6 @@
 const CHANGELOG = [
     // { version: '', log: '' },
+    { version: 'v0.16.4', log: 'Re-added murderBlock.png' },
     { version: 'v0.16.3', log: 'Removed murderblock.png' },
     { version: 'v0.16.2', log: 'Quit button goes to sharkside homepage' },
     { version: 'v0.16.1', log: 'Main menu background + music, button SFX' },

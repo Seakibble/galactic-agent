481e1e7fb8cc580aeeab5e1d117a399ae23d8a45
Max Leeming <max.leeming@gmail.com>
2023-08-15 23:06:46 -0400

    v0.17.1 - Updated planet name, style tweaks

--- game/changelog.js
+++ game/changelog.js
@@ -1,5 +1,6 @@
 const CHANGELOG = [
     // { version: '', log: '' },
+    { version: 'v0.17.1', log: 'Updated planet name, style tweaks' },
     { version: 'v0.17.0', log: 'Added an opening cutscene' },
     { version: 'v0.16.6', log: 'Added tasteful fade between screens/levels' },
     { version: 'v0.16.5', log: 'Fixed quit/main menu links' },
